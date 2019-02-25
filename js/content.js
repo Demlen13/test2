e971e771752ec0a780a8d29a8156057c5600a9fd
laposa.daniel <laposa.daniel@C302-13>
2019-02-25 17:06:45 +0100

    20190225

---
+++ js/content.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Content {
+    content(req, res) {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<h1>Hello bello izé</h1>");
+        res.end();
+    }
+}
+exports.default = Content;
+//# sourceMappingURL=content.js.map
---
+++ ts/content.ts
@@ -0,0 +1,10 @@
+﻿import * as http from "http";
+
+export default class Content {
+
+    public content(req: http.IncomingMessage, res: http.ServerResponse): void {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<h1>Hello bello izé</h1>");
+        res.end();
+    }
+}
